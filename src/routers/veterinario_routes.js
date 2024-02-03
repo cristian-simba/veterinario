ca881ae511d3d01944e0a4ae4086476ed24f682a
cristian-simba <davidsimba200@gmail.com>
2024-02-03 17:57:54 -0500

    prueba de carga

--- src/routers/veterinario_routes.js
+++ src/routers/veterinario_routes.js
@@ -19,17 +19,20 @@ import { validacionVeterinario } from '../middlewares/validacionVeterinario.js';
 const router = Router()
 
 //publicas 
-router.post("/login", login);
-router.post("/registro",validacionVeterinario, registro);
-router.get("/confirmar/:token", confirmEmail);
+router.post("/login", (req, res) => {
+    res.send('Bienvenido');});
+router.post("/registro", registro);
+router.get("/confirmar/123", (req, res) => {
+    res.send('Confirmar Cuenta');});
 router.get("/veterinarios", listarVeterinarios);
 router.get("/recuperar-password", recuperarPassword);
-router.get("/recuperar-password/:token", comprobarTokenPasword);
-router.post("/nuevo-password/:token", nuevoPassword);
+router.get("/recuperar-password/123", comprobarTokenPasword);
+router.post("/nuevo-password/123", nuevoPassword);
 
 
 //privadas
-router.get("/perfil",verificarAutenticacion, perfil);
+router.get("/perfil", (req, res) => {
+    res.send('Bienvenido Al Perfil');});
 router.put('/veterinario/actualizarpassword',verificarAutenticacion,actualizarPassword)
 router.get("/veterinario/:id",verificarAutenticacion ,detalleVeterinario);
 router.put("/veterinario/:id",verificarAutenticacion ,actualizarPerfil);
